fcaaa0d969ef2c979e734ab402534a6d8abfae02
sohrabeghasemi <sohrabeghasemi@gmail.com>
2019-04-07 01:45:01 -0700

    add personal 2 fields

--- web/models.py
+++ web/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class personel(models.Model):
+
+	MADRAK_CHOICES = {
+		'ci': 'سیکل',
+		'dp': 'دیپلم',
+		'fd': 'فوق دیپلم',
+		'li': 'لیسانس',
+		'fl': 'فوق لیسانس',
+		'dr': 'دکترا',
+	}
+
+	F_name = models.CharField(max_length=200)
+	L_name = models.CharField(max_length=250)
+
+
